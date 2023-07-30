f127204a5d49aa5a193f9593b3d7b248346b26ec
Andrii Shokhirev <andresshokhirev@gmail.com>
2023-07-30 12:07:30 +0300

    Add arithmetic methods to Calculator

--- calculator-git-demo/src/Calculator.java
+++ calculator-git-demo/src/Calculator.java
@@ -15,4 +15,13 @@ public class Calculator {
         }
         return -1;
     }
+    public double pow(double a, int b) {
+        if (b == 0) {
+            return 1;
+        }
+        return a * pow(a, b - 1);
+    }
+    public double sqrt(double a) {
+        return Math.sqrt(a);
+    }
 }
--- calculator-git-demo/src/Main.java
+++ calculator-git-demo/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculator cal = new Calculator();
+
+        cal.sum(1.0, 2.0);
+        cal.subtracting(15.0, 23.5);
+        cal.multiplication(13.2, 23.1);
+        cal.division(95.2, 18.0);
+
+        cal.pow(3, 6);
+        cal.sqrt(16);
     }
 }
