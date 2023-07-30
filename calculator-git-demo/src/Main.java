7d7bca0ad0c3f20600d65242779d4406b860982b
Andrii Shokhirev <andresshokhirev@gmail.com>
2023-07-30 12:17:31 +0300

    Add output to Main class

--- calculator-git-demo/src/Main.java
+++ calculator-git-demo/src/Main.java
@@ -2,12 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Calculator cal = new Calculator();
 
-        cal.sum(1.0, 2.0);
-        cal.subtracting(15.0, 23.5);
-        cal.multiplication(13.2, 23.1);
-        cal.division(95.2, 18.0);
-
-        cal.pow(3, 6);
-        cal.sqrt(16);
+        System.out.println(cal.sum(1.0, 2.0));
+        System.out.println(cal.subtracting(15.0, 23.5));
+        System.out.println(cal.multiplication(13.2, 23.1));
+        System.out.println(cal.division(95.2, 18.0));
+        System.out.println(cal.pow(3, 6));
+        System.out.println(cal.sqrt(16));
     }
 }
